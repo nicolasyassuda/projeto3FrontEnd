ae326bab6abcaa68ae8af4175b039ceec7f40c47
Nicolas Yassuda <nicolas.yassuda@smartbrain.com.br>
2023-05-18 09:35:32 -0300

    login Implemented

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -4,8 +4,11 @@ export default function login() {
     return (
         <div className={styles["background"]}>
             <img alt={"backgrounImage"} src={"/imgs/backgroundImage.jpg"} style={{ width: "100vw", height: "100vh" }}></img>
-            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", position: "absolute", top: "0px", left: "0px", width: "100vw", height: "100vh", backgroundColor: "rgba(0,0,0,0.5)" }}>
-                <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", width: "30vw", height: "70vh", backgroundColor: "rgba(0,0,0,1)", borderRadius: "2vw", boxShadow: "-0vw 0vw 3vw gray" }}>
+            <div>
+
+            </div>
+            <div className={styles["container"]}>
+                <div className={styles["login"]}>
                     <div>
                         <p style={{ color: "gray" }}>E-mail</p>
                         <input type="email" placeholder="example@mail.com" className={styles["input"]}></input>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,47 +2,50 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-
+import { useState } from 'react'
+import Login from '@/components/login'
 export default function Home() {
-
+  const [autenticado, setAutenticado] = useState<boolean>(false);
   function renderizarImagens() {
     const listaImagens = [1, 2, 3, 4, 5]
     return listaImagens.map((imagem) => {
       return (
-        <div style={{ flexWrap: "wrap", width: "20vw", cursor: "pointer", marginBottom:'4vh' }}>
-
+        <div style={{ flexWrap: "wrap", width: "20vw", cursor: "pointer", marginBottom: '4vh' }}>
           <img
             className={styles["imagem"]}
             src="/imgs/capa.jpg"
             height={300} // Desired size with correct aspect ratio
             width={250} // Desired size with correct aspect ratio
           />
-
         </div>
       )
     })
   }
-  function header(){
-    return(
+  function autenticar(){
+    
+  }
+  function header() {
+    return (
       <div className="bg-cyan-400 h-20">
       </div>
     )
   }
   return (
-    <div style={{ display: "flex", flexDirection: "column", marginTop: "1vw", width: "100vw",marginLeft:"2vw", justifyContent: "center", alignItems: "center" }}>
-      <div>
-        {header()}
-      </div>
-      <div style={{ display: 'grid', marginTop: '2vh', gridTemplateColumns: "20vw 20vw 20vw 20vw 20vw" }}>
+    <div>
+      {autenticado ? (<div style={{ display: "flex", flexDirection: "column", marginTop: "1vw", width: "100vw", marginLeft: "2vw", justifyContent: "center", alignItems: "center" }}>
+        <div>
+          {header()}
+        </div>
+        <div style={{ display: 'grid', marginTop: '2vh', gridTemplateColumns: "20vw 20vw 20vw 20vw 20vw" }}>
 
-        {renderizarImagens()}
-        {renderizarImagens()}
-        {renderizarImagens()}
-        {renderizarImagens()}
-        {renderizarImagens()}
+          {renderizarImagens()}
+          {renderizarImagens()}
+          {renderizarImagens()}
+          {renderizarImagens()}
+          {renderizarImagens()}
 
-      </div>
+        </div>
+      </div>) : (<><Login></Login></>)}
     </div>
-
   )
 }
