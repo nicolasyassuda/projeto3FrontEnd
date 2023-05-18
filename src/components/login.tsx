49de393bc33e9e06e8c8a3aaa7f0fe8293a3e91e
Nicolas Yassuda <nicolas.yassuda@smartbrain.com.br>
2023-05-18 09:08:45 -0300

    Changes

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,9 +1,22 @@
 import styles from "../styles/login.module.css"
-
-export default function login(){
-    return(
+import Image from "next/image"
+export default function login() {
+    return (
         <div className={styles["background"]}>
-
+            <img alt={"backgrounImage"} src={"/imgs/backgroundImage.jpg"} style={{ width: "100vw", height: "100vh" }}></img>
+            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", position: "absolute", top: "0px", left: "0px", width: "100vw", height: "100vh", backgroundColor: "rgba(0,0,0,0.5)" }}>
+                <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", width: "30vw", height: "70vh", backgroundColor: "rgba(0,0,0,1)", borderRadius: "2vw", boxShadow: "-0vw 0vw 3vw gray" }}>
+                    <div>
+                        <p style={{ color: "gray" }}>E-mail</p>
+                        <input type="email" placeholder="example@mail.com" className={styles["input"]}></input>
+                    </div>
+                    <div>
+                        <p style={{ color: "gray" }}>Password</p>
+                        <input type="password" placeholder="password" className={styles["input"]}></input>
+                    </div>
+                    <button className={styles["btn"]}>Entrar</button>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
+import Login from '@/components/login'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main>
-      <body>
-        <div>
-          
-        </div>
-      </body>
-    </main>
+    <div style={{margin:"0px"}}>
+      <Login></Login>
+    </div>
   )
 }
