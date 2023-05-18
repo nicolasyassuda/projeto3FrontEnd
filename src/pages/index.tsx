6e6dfce38287b652fc591ba5a35e7d72339c935d
GabrielaKimi <81189960+GabrielaKimi@users.noreply.github.com>
2023-05-18 08:07:52 -0400

    Estilo pagina inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,46 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
-  return (
-    <main>
-      <body>
-        <div>
-          
+
+  function renderizarImagens() {
+    const listaImagens = [1, 2, 3, 4, 5]
+    return listaImagens.map((imagem) => {
+      return (
+        <div style={{ flexWrap: "wrap", width: "20vw", cursor: "pointer", marginBottom:'4vh' }}>
+
+          <img
+            className={styles["imagem"]}
+            src="/imgs/capa.jpg"
+            height={300} // Desired size with correct aspect ratio
+            width={250} // Desired size with correct aspect ratio
+          />
+
         </div>
-      </body>
-    </main>
+      )
+    })
+  }
+  function header(){
+    return(
+      <div className="bg-cyan-400 h-20">
+      </div>
+    )
+  }
+  return (
+    <div style={{ display: "flex", flexDirection: "column", marginTop: "1vw", width: "100vw",marginLeft:"2vw", justifyContent: "center", alignItems: "center" }}>
+      <div>
+        {header()}
+      </div>
+      <div style={{ display: 'grid', marginTop: '2vh', gridTemplateColumns: "20vw 20vw 20vw 20vw 20vw" }}>
+
+        {renderizarImagens()}
+        {renderizarImagens()}
+        {renderizarImagens()}
+        {renderizarImagens()}
+        {renderizarImagens()}
+
+      </div>
+    </div>
+
   )
 }
