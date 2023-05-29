7ae4b35a422f8c0a0b93a7159c888f1e3abdd3c3
Gustavo <gustavoms7@al.insper.edu.br>
2023-05-29 19:53:33 -0300

    semCarrousel

--- src/pages/filmeSelecionado.tsx
+++ src/pages/filmeSelecionado.tsx
@@ -1,64 +1,56 @@
 import React, { useState } from 'react';
 import styles from '@/styles/Home.module.css';
-import Carousel from '@/components/Carrousel';
+
 import Header from '@/components/header';
 
 export default function FilmeSelecionado() {
-  // Estado para controlar se o filme é favorito ou não
   const [favorito, setFavorito] = useState(false);
-
-  // Estado para armazenar a avaliação do filme
   const [avaliacao, setAvaliacao] = useState(0);
+  
 
-  // Função para lidar com o clique no botão de favorito
   const handleFavoritoClick = () => {
     setFavorito(!favorito);
   };
 
-  // Função para lidar com a mudança na seleção de avaliação
   const handleAvaliacaoChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedValue = parseInt(event.target.value);
     setAvaliacao(selectedValue);
   };
 
-  // Array de atores
-  const atores = ['Ator 1', 'Ator 2', 'Ator 3'];
-
   return (
-    <div>
-      <Header name="Nome do Site">
-        Conteúdo adicional aqui...
-      </Header>
+    <div className={styles.container}>
+      <Header name="Nome do Site" />
+      <h1 className={styles.nomeFilme}>Filme Selecionado</h1>
+      <img className={styles.poster} src="/imgs/capa.jpg" alt="Imagem do filme" />
 
-      <div className={styles['imagem-container']}>
-      <h1 className ={styles.Titulo} >Filme Selecionado</h1>
-        '<img
-            className={styles.imagem}
-            src="/imgs/capa.jpg"
-            alt="Imagem do filme"
-        />
-      </div>'
-
-
-      <div className={styles['infos-container']}>
-        {/* Componente Carousel que renderiza os atores */}
-        <Carousel items={atores} />
+      <div className={styles.infos}>
+        {"/* Primeiro container de informações */"}
+        <div className={`${styles.infoContainer} ${styles.firstInfoContainer}`}>
+          {"/* Conteúdo do primeiro container */"}
+        </div>
       </div>
+
+      <div className={styles.infos}>
+        {"/* Segundo container de informações */"}
+        <div className={`${styles.infoContainer} ${styles.secondInfoContainer}`}>
+          {"/* Conteúdo do segundo container */"}
+        </div>
+      </div>
+
       <div className={styles['rating-container']}>
-        <p>Avaliação:</p>
         <select value={avaliacao} onChange={handleAvaliacaoChange}>
-          <option value={0}>Selecione</option>
+          <option value={0}>Avaliação</option>
           <option value={1}>1</option>
           <option value={2}>2</option>
           <option value={3}>3</option>
           <option value={4}>4</option>
           <option value={5}>5</option>
         </select>
+
+        <button onClick={handleFavoritoClick}>
+          {favorito ? 'Remover dos Favoritos' : 'Adicionar aos Favoritos'}
+        </button>
       </div>
-      <button onClick={handleFavoritoClick}>
-        {favorito ? 'Remover dos Favoritos' : 'Adicionar aos Favoritos'}
-      </button>
     </div>
-  );  
-  
+  );
 }
